33beb58463517f292f954442469045fd8607dc1d
Peter Andersson <maxnicoandersson@gmail.com>
2018-12-23 13:13:40 +0100

    Updated unit tests for pot

--- src/test/java/poker/TestPot.java
+++ src/test/java/poker/TestPot.java
@@ -11,7 +11,7 @@ import static junit.framework.TestCase.assertEquals;
 public class TestPot {
 
   @Test
-  public void testTwoPlayersOneAllIn() {
+  public void testTwoPlayersOnePotSplit() {
     Pot pot = new Pot();
     Player peter = new RobotPlayer("Peter", 500);
     Player thomas = new RobotPlayer("Thomas", 100);
@@ -22,16 +22,18 @@ public class TestPot {
     pot.addMember(thomas, 100);
 
    final Pot newPot = pot.splitPot(100);
-    assertEquals(pot.getMembers(), oldPotMembers);
-    assertEquals(pot.getNumberOfMarkers(), 200);
-    assertEquals(newPot.getNumberOfMarkers(), 400);
+    assertEquals(oldPotMembers, pot.getMembers());
+    assertEquals(200, pot.getNumberOfMarkers());
+    assertEquals(100, pot.getMarkersForMember(peter));
+    assertEquals(100, pot.getMarkersForMember(thomas));
+    assertEquals(400, newPot.getNumberOfMarkers());
     final List<Player> newPotMemberList = Lists.newArrayList();
     newPotMemberList.add(peter);
     assertEquals(newPot.getMembers(), newPotMemberList);
   }
 
   @Test
-  public void testThreePlayersTwoSplitsOneAllIn() {
+  public void testThreePlayersTwoPotSplits() {
     Pot pot = new Pot();
     Player peter = new RobotPlayer("Peter", 500);
     Player thomas = new RobotPlayer("Thomas", 100);
@@ -45,16 +47,21 @@ public class TestPot {
     pot.addMember(ingemar, 1000);
 
     final Pot newPot = pot.splitPot(100);
-    assertEquals(pot.getMembers(), oldPotMembers);
-    assertEquals(pot.getNumberOfMarkers(), 300);
+    assertEquals(oldPotMembers, pot.getMembers());
+    assertEquals(300, pot.getNumberOfMarkers());
+    assertEquals(100, pot.getMarkersForMember(peter));
+    assertEquals(100, pot.getMarkersForMember(thomas));
+    assertEquals(100, pot.getMarkersForMember(ingemar));
     assertEquals(newPot.getNumberOfMarkers(), 1300);
 
     final List<Player> newPotMemberList = Lists.newArrayList();
     newPotMemberList.add(peter);
     newPotMemberList.add(ingemar);
     assertEquals(newPot.getMembers(), newPotMemberList);
+    assertEquals(400, newPot.getMarkersForMember(peter));
+    assertEquals(900, newPot.getMarkersForMember(ingemar));
 
-    final Pot newestPot = newPot; // TODO does not work
+    final Pot newestPot = newPot.splitPot(400);
     List<Player> newestPotMembers = Lists.newArrayList();
     newestPotMembers.add(ingemar);
     assertEquals(newestPotMembers, newestPot.getMembers());
@@ -71,7 +78,24 @@ public class TestPot {
     pot.addMember(peter, 500);
     pot.addMember(thomas, 100);
     pot.addMember(ingemar, 1000);
-    //Pot pot = pot.splitPot(200);
-    //assertEquals(pot);
+    Pot newPot = pot.splitPot(200);
+    List<Player> newPotMembers = Lists.newArrayList();
+    newPotMembers.add(peter);
+    newPotMembers.add(ingemar);
+    List<Player> oldPotMembers = Lists.newArrayList();
+    oldPotMembers.add(thomas);
+    oldPotMembers.add(peter);
+    oldPotMembers.add(ingemar);
+    assertEquals(oldPotMembers, pot.getMembers());
+    assertEquals(500, pot.getNumberOfMarkers());
+    assertEquals(200, pot.getHighestAmount());
+    assertEquals(200, pot.getMarkersForMember(peter));
+    assertEquals(100, pot.getMarkersForMember(thomas));
+    assertEquals(200, pot.getMarkersForMember(ingemar));
+    assertEquals(newPotMembers, newPot.getMembers());
+    assertEquals(1100, newPot.getNumberOfMarkers());
+    assertEquals(300, newPot.getMarkersForMember(peter));
+    assertEquals(800, newPot.getMarkersForMember(ingemar));
+    assertEquals(800, newPot.getHighestAmount());
   }
 }
